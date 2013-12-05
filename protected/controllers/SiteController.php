c307c8dc29cd1a6ed09e6803c16b5f2c07dbcf94
蔡正海 <caizhenghai@gmail.com>
2013-12-05 17:21:39 +0800

    添加微信测试

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -2,6 +2,8 @@
 
 class SiteController extends Controller
 {
+	private $_weixinToken = "forecho";
+
 	/**
 	 * Declares class-based actions.
 	 */
@@ -32,52 +34,45 @@ class SiteController extends Controller
 		$this->render('index');
 	}
 
-
-	/**
-     * actionIndex 
-     * 
-     * @access public
-     * @return void
-     */
-    public function actionWinxin()
-    {
-        $weixin = new Weixin($_GET);
-        $weixin->token = $this->_weixinToken;
-        //$weixin->debug = true;
- 
-        //网址接入时使用
-        if (isset($_GET['echostr']))
-        {
-            $weixin->valid();
-        }
-         
-        $weixin->init();
-        $reply = '';
-        $msgType = empty($weixin->msg->MsgType) ? '' : strtolower($weixin->msg->MsgType);
-        switch ($msgType)
-        {
-        case 'text':
-            //你要处理文本消息代码
-            break;
-        case 'image':
-            //你要处理图文消息代码
-            break;
-        case 'location':
-            //你要处理位置消息代码
-            break;
-        case 'link':
-            //你要处理链接消息代码
-            break;
-        case 'event':
-            //你要处理事件消息代码
-            break;
-        default: 
-            //无效消息情况下的处理方式
-            break;
-        }
-        $weixin->reply($reply);
-    }
-
+   	public function actionWeixin()
+    {
+    	Yii::createComponent('application.extensions.weixin.weixin');
+        $weixin = new Weixin($_GET);
+        $weixin->token = $this->_weixinToken;
+        $weixin->debug = true;
+ 
+        //网址接入时使用
+        if (isset($_GET['echostr']))
+        {
+            $weixin->valid();
+        }
+         
+        $weixin->init();
+        $reply = '';
+        $msgType = empty($weixin->msg->MsgType) ? '' : strtolower($weixin->msg->MsgType);
+        switch ($msgType)
+        {
+        case 'text':
+            //你要处理文本消息代码
+            break;
+        case 'image':
+            //你要处理图文消息代码
+            break;
+        case 'location':
+            //你要处理位置消息代码
+            break;
+        case 'link':
+            //你要处理链接消息代码
+            break;
+        case 'event':
+            //你要处理事件消息代码
+            break;
+        default: 
+            //无效消息情况下的处理方式
+            break;
+        }
+        $weixin->reply($reply);
+    }
 
 	/**
 	 * This is the action to handle external exceptions.

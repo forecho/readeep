839cd79231126c9d94a3bd45bb9834347ae205e3
forecho <caizhenghai@gmail.com>
2014-01-18 14:11:44 +0800

    添加禁止缩放

--- themes/foundation/views/layouts/main.php
+++ themes/foundation/views/layouts/main.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densitydpi=device-dpi" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
 	<link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/foundation.min.css" rel="stylesheet">

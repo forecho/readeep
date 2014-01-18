e45726a37d50aa13eb43813b7861ccf014895c98
forecho <caizhenghai@gmail.com>
2014-01-18 14:25:02 +0800

    修改大小写 敏感 bug

--- protected/admin/controllers/ElfinderController.php
+++ protected/admin/controllers/ElfinderController.php
@@ -6,7 +6,7 @@ class ElfinderController extends CController
     {
         return array(
             'connector' => array(
-                'class' => 'ext.yii-elFinder.ElFinderConnectorAction',
+                'class' => 'ext.yii-elfinder.ElFinderConnectorAction',
                 'settings' => array(
                     'root' => Yii::getPathOfAlias('webroot') . '/uploads/'.Yii::app()->user->id.'/',
                     'URL' => Yii::app()->baseUrl . '/uploads/'.Yii::app()->user->id.'/',

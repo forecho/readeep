9859f4697ef2b618685975708ac7c505b37130d0
forecho <caizhenghai@gmail.com>
2014-01-18 15:01:20 +0800

    修改导航

--- themes/foundation/views/layouts/main.php
+++ themes/foundation/views/layouts/main.php
@@ -70,6 +70,17 @@
 	    </nav>
 
 	    <aside class="left-off-canvas-menu">
+	    	<?php $this->widget('zii.widgets.CMenu',array(
+				'items'=>array(
+					array('label'=>'Home', 'url'=>array('/site/index')),
+					array('label'=>'Post', 'url'=>array('/postPosts/index')),
+					array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+					array('label'=>'Contact', 'url'=>array('/site/contact')),
+					array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+					array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+				),
+				'htmlOptions' => array('class'=>'off-canvas-list'),
+			)); ?>
 			<ul class="off-canvas-list">
 				<li><label>Foundation</label></li>
 				<li><a href="/site/index">Home</a></li>

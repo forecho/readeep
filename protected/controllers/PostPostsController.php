d40e84fa6ea142c51d657736ce4c7ed4df3121bc
forecho <caizhenghai@gmail.com>
2014-01-19 18:08:58 +0800

    测试权限系统

--- protected/controllers/PostPostsController.php
+++ protected/controllers/PostPostsController.php
@@ -29,7 +29,7 @@ class PostPostsController extends Controller
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('addLike','addThanks'),
-				'users'=>array('@'),
+				'users'=>array('?'),
 			),
 			array('deny',  // deny all users
 	            'users'=>array('*'),

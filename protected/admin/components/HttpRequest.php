ab4cf29b4f443c3167684292735e55c004dbf708
forecho <caizhenghai@gmail.com>
2014-01-19 22:40:53 +0800

    测试解决 Chrome 导致后台编辑器的 js 错误

--- protected/admin/components/HttpRequest.php
+++ protected/admin/components/HttpRequest.php
@@ -3,16 +3,18 @@
 * @Author: caicai
 * @Date:   2014-01-19 21:03:58
 * @Email:  email@example.com
-* @Last modified by:   caicai
-* @Last modified time: 2014-01-19 21:04:06
+* @Last modified by:   forecho
+* @Last modified time: 2014-01-19 22:40:38
 */
 
 class HttpRequest extends CHttpRequest
 {
-    public function validateCsrfToken($event)
-    {
-        $contentType = isset($_SERVER["CONTENT_TYPE"]) ? $_SERVER["CONTENT_TYPE"] : null;
-        if ($contentType !== 'application/json')
+
+   public $dont_validate_csrf_routes = array();
+
+   public function validateCsrfToken($event) {
+        if( !in_array(Yii::app()->request->url, $this->dont_validate_csrf_routes) ) {
             parent::validateCsrfToken($event);
+        }
     }
 }

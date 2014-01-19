0a3a1739506b026dfb6f1be3ba8fdf5049ada73c
forecho <caizhenghai@gmail.com>
2014-01-19 18:02:04 +0800

    点赞和感情加入权限系统

--- protected/controllers/PostPostsController.php
+++ protected/controllers/PostPostsController.php
@@ -5,6 +5,17 @@ class PostPostsController extends Controller
 	public $layout='//layouts/column1';
 
 	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+			'postOnly + delete', // we only allow deletion via POST request
+		);
+	}
+
+	/**
 	 * Specifies the access control rules.
 	 * This method is used by the 'accessControl' filter.
 	 * @return array access control rules
@@ -20,6 +31,9 @@ class PostPostsController extends Controller
 				'actions'=>array('addLike','addThanks'),
 				'users'=>array('@'),
 			),
+			array('deny',  // deny all users
+	            'users'=>array('*'),
+	        ),
 		);
 	}
 

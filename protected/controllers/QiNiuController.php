773bdb4c085d1ea447c35b8a2a83d4f83b11ebf2
蔡正海 <caizhenghai@gmail.com>
2014-03-06 16:11:52 +0800

    美化代码格式

--- protected/controllers/QiNiuController.php
+++ protected/controllers/QiNiuController.php
@@ -1,29 +1,28 @@
 <?php
 
-        class QiNiuController extends Controller
-        {
+class QiNiuController extends Controller
+{
 
-                public function actions()
-                {
-                        return array(
-                            // captcha action renders the CAPTCHA image displayed on the contact page
-                            'captcha' => array(
-                                'class'     => 'CCaptchaAction',
-                                'backColor' => 0xFFFFFF,
-                            ),
-                            // page action renders "static" pages stored under 'protected/views/site/pages'
-                            // They can be accessed via: index.php?r=site/page&view=FileName
-                            'page'    => array(
-                                'class' => 'CViewAction',
-                            ),
-                        );
-                }
+    public function actions()
+    {
+        return array(
+            // captcha action renders the CAPTCHA image displayed on the contact page
+            'captcha' => array(
+                'class'     => 'CCaptchaAction',
+                'backColor' => 0xFFFFFF,
+            ),
+            // page action renders "static" pages stored under 'protected/views/site/pages'
+            // They can be accessed via: index.php?r=site/page&view=FileName
+            'page'    => array(
+                'class' => 'CViewAction',
+            ),
+        );
+    }
 
-                public function actionIndex()
-                {
-                        
-                       $obj=new QiNiuClound();
-                       $this->render('index',array('token'=>$obj->getUpToken()));
-                   }
+    public function actionIndex()
+    {
+       $obj=new QiNiuClound();
+       $this->render('index',array('token'=>$obj->getUpToken()));
+    }
 
-        }
+}

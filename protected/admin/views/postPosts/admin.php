30a8b6956fffc1791fcb40b4fb66ec48044136b3
forecho <caizhenghai@gmail.com>
2014-03-25 23:16:13 +0800

    创建文章添加作者字段 实现上传图片功能 但是体验不好

--- protected/admin/views/postPosts/admin.php
+++ protected/admin/views/postPosts/admin.php
@@ -39,8 +39,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 )); ?>
 </div><!-- search-form -->
 
-<?php 
-$this->widget('zii.widgets.grid.CGridView', array(
+<?php $this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'post-posts-grid',
 	'dataProvider'=>$model->search(),
 	'pager' => array(
